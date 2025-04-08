765c7655064eca554847644d160b96d645c7a9ea
Arif <arif@themascoteers.com>
2025-04-08 15:48:24 +1000

    done

--- mat_to_npy.py
+++ mat_to_npy.py
@@ -1,13 +1,13 @@
 import numpy as np
 from scipy.io import loadmat
 import os
+import os
 
 
-def convert_mat_to_npy(mat_file_path):
+def convert_mat_to_npy(mat_file_path, save_dir):
     data = loadmat(mat_file_path)
     data = {k: v for k, v in data.items() if not k.startswith('__')}
 
-    save_dir = os.path.dirname(mat_file_path)
     os.makedirs(save_dir, exist_ok=True)
 
     for key, value in data.items():
@@ -16,5 +16,9 @@ def convert_mat_to_npy(mat_file_path):
         print(f"Saved {key} to {npy_path}")
 
 
-mat_file_path = "datasets/samson/end3.mat"
-convert_mat_to_npy(mat_file_path)
+mat_file_path = "mat_datasets/samson_1.mat"
+gt_file_path = "mat_datasets/end3.mat"
+
+os.makedirs("tempwork", exist_ok=True)
+
+convert_mat_to_npy(mat_file_path, "tempwork")
