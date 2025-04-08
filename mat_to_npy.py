fc9082f61c496a80ca1840b64ac4cdb85a0fecf2
Arif <arif@themascoteers.com>
2025-04-08 15:53:08 +1000

    done

--- mat_to_npy.py
+++ mat_to_npy.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.io import loadmat
 import os
 import os
+import shutil
 
 
 def convert_mat_to_npy(mat_file_path, save_dir):
@@ -16,9 +17,15 @@ def convert_mat_to_npy(mat_file_path, save_dir):
         print(f"Saved {key} to {npy_path}")
 
 
+os.makedirs("datasets/samson", exist_ok=True)
+
 mat_file_path = "mat_datasets/samson_1.mat"
 gt_file_path = "mat_datasets/end3.mat"
 
 os.makedirs("tempwork", exist_ok=True)
 
 convert_mat_to_npy(mat_file_path, "tempwork")
+shutil.copy("tempwork/V.npy","datasets/samson/data.npy")
+
+convert_mat_to_npy(gt_file_path, "tempwork")
+shutil.copy("tempwork/A.npy","datasets/samson/gt.npy")
